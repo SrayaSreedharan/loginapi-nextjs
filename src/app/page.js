341cb622d8fe445541457411d1d6f63f0841032a
SRAYA SREEDHARAN <srayasreedharan111@gmail.com>
2025-07-17 20:35:22 +0530

    start

--- src/app/page.js
+++ src/app/page.js
@@ -29,16 +29,10 @@ export default function LoginForm() {
       <form onSubmit={handleSubmit} className="bg-white shadow-xl rounded-2xl p-8 w-full max-w-md space-y-6">
         <h2 className="text-2xl font-bold text-center text-gray-800">Login</h2>
         <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} required className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"/>
-
         <input type="password" placeholder="Password" value={password}onChange={(e) => setPassword(e.target.value)} required className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"/>
-
-        <button
-          type="submit"
-          className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition duration-200"
-        >
+        <button type="submit" className="w-full bg-blue-600 text-white py-2 rounded-lg hover:bg-blue-700 transition duration-200">
           Login
         </button>
-
         {message && (
           <p
             className={`text-center text-sm ${
