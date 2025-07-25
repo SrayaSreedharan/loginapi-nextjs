d941fbf5dfcaaf904d16e3b200478c28e261ebca
SRAYA SREEDHARAN <srayasreedharan111@gmail.com>
2025-07-25 15:11:01 +0530

    test

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import Header from './component/Header'
+import Footer from './component/Footer'
 
 const page = () => {
   return (
     <div>
+      <Header/>
       home page
+      <Footer/>
     </div>
   )
 }
--- src/middleware.js
+++ src/middleware.js
@@ -2,11 +2,11 @@ import { NextResponse } from 'next/server'
  
 
 export function middleware(request) {
-  return NextResponse.redirect(new URL('/admin/dashboard', request.url))
+  // return NextResponse.redirect(new URL('/admin', request.url))
 
-  if (role !== 'admin') {
-      return NextResponse.redirect(new URL('/login', request.url));
-    }
+  // if (role !== 'admin') {
+  //     return NextResponse.redirect(new URL('/login', request.url));
+  //   }
 }
 export const config = {
    matcher: '/admin/:path*',
